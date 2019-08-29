caf3438a11558e190726c12d9c200d3ad9f7ba95
sbenselum <54150427+sbenselum@users.noreply.github.com>
2019-08-29 17:04:36 -0300

    Add files via upload

---
+++ sudoku.js
@@ -0,0 +1,42 @@
+function sudokuGames(){
+    game[
+        0,0,0, 8,7,2, 0,0,1,
+        7,8,5, 0,0,0, 0,0,0,
+        0,0,0, 0,0,5, 8,0,0,
+
+        5,0,3, 0,0,0, 9,6,0,
+        1,0,8, 0,0,0, 2,0,7,
+        0,9,6, 0,0,0, 1,0,8,
+
+        0,0,1, 5,0,0, 0,0,0,
+        0,0,0, 0,0,0, 7,1,9,
+        8,0,0, 6,9,1, 0,0,0];
+
+    return game;
+}        
+function pressButton(id){
+    var valueOnDiv = document.getElementById(id).innerHTML;
+    var sudoku = [];
+    
+    // Puts value on the defined square
+    if(valueOnDiv == ''){
+        document.getElementById(id).innerHTML = markValue;
+    }
+
+    // Builds array with values on screen
+    for(i = 1; i <= 9; i++ ){
+        idTateti = 'sudoku'+i;
+        tateti.push(document.getElementById(idTateti).innerHTML);
+    }
+        
+}
+
+function buildTable(){
+    for(n = 1; n <= 9; n++ ){
+        let textGrid = '';
+        for(i = 1; i <= 9; i++ ){
+            textGrid = textGrid+"<div id='sudoku"+n+'-'+i+"' onClick='pressButton(this.id)'></div>";
+        }
+        document.getElementById('sudoku'+n).innerHTML = textGrid;
+    }
+}   
